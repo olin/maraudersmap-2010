728fb896978f2c3c043e8734042a009393ee93f8
Julian Ceipek <julian.ceipek@gmail.com>
2012-02-25 16:06:11 -0500

    Added a lot of comments

--- olinmm-1.1.0/binutils/coordinates.py
+++ olinmm-1.1.0/binutils/coordinates.py
@@ -3,7 +3,7 @@ import sys
 
 def getavgcoords(n=3, tsleep = 0.15):
     import time
-    totals = {}
+    totals = dict()
     for i in range(n):
         print i
         res = getcoordinates()
@@ -24,8 +24,20 @@ def getavgcoords(n=3, tsleep = 0.15):
 def getcoordinates():
     binutilspath = getpath()
     coord = Coordinate()
-    ret = {}
-    if sys.platform=='win32':
+
+    ret = dict()
+    # Dictionary with key : value pairs of the form
+    # MAC_Address : [Signal_Strength, Network_Name]
+    # Example:
+    # {
+    #   '00:20:D8:2D:2C:C1': [14, 'OLIN_CC'],
+    #   '00:20:D8:2D:B3:C0': [12, 'OLIN_CC'],
+    #   '00:20:D8:2D:65:02': [12, 'OLIN_WH'],
+    #   '00:20:D8:2D:85:40': [38, 'OLIN_CC']
+    # }
+
+    # WINDOWS #FIXME: untested since the MM went offline #XXX: Won't work any longer
+    if sys.platform.startswith('win'):
         o = os.popen(os.path.join(binutilspath, 'getcoords.exe'))
         res = o.read().replace('\r\n','\n').split('\n')
         o.close()
@@ -39,7 +51,8 @@ def getcoordinates():
                 coord.name = linepts[2]
                 
                 ret[coord.mac] = [coord.strength, coord.name]
-    elif sys.platform=='linux2':
+    # LINUX #FIXME: untested since the MM went offline
+    elif sys.platform.startswith('linux'):
         o = os.popen(binutilspath + 'mmscan')
         res = o.read().replace('\r\n','\n').split('\n')
         o.close()
@@ -66,39 +79,58 @@ def getcoordinates():
                 if 'OLIN' in coord.name and 'GUEST' not in coord.name:
                     ret[coord.mac] = [coord.strength, coord.name]
             count = count + 1
-    elif sys.platform=='darwin':
+    # MAC OS X
+    elif sys.platform.startswith('darwin'):
         import plistlib
         cmd = '/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -s -x'
-        ntwks = []
+        # This is an undocumented system utility available on Mac OS X
+        # From the included help:
+        # -s[<arg>] --scan=[<arg>]       Perform a wireless broadcast scan.
+        #		   Will perform a directed scan if the optional <arg> is provided
+        # -x        --xml                Print info as XML
+        ntwks = list()
         try:
+            # Get information about networks 
             ntwks = plistlib.readPlist(os.popen(cmd))
-        except:
+        except Exception as e:
             print "Failed to find networks.  The command '%s' may not exist." % cmd
+            print "Traceback is:\n%s" % e            
         
         for network in ntwks:
             if 'OLIN' in network['SSID_STR'] and 'GUEST' not in network['SSID_STR']:
+                # Now we are pretty sure that this is a non-guest Olin network
+                # Unless someone else has a router with 'OLIN' in the SSID
+                
+                # The BSSID (MAC address) is of the form 0:20:d8:2d:65:2
+                # Now we need to convert it to the format 00:20:D8:2D:65:20
                 macbytes = network['BSSID'].split(':')
-                bssid = []
+                bssid = list()
+                print 'RSSI:', network['RSSI']
                 for byte in macbytes:
                     if len(byte) < 2:
                         bssid.append(('0%s' % byte).upper())
                     else:
                         bssid.append(byte.upper())
                 ret[':'.join(bssid)] = [interpretDB(network['RSSI']), network['SSID_STR']]
+    print "ret",ret
     return ret
     
 class Coordinate:
+    # XXX: The way this class is used is really silly. It should probably die. - Julian
     name = ''
     strength = ''
     mac = ''
 
 def interpretDB(string):
+    # All platforms return the Received Signal Strength Indication (RSSI) in dBm units (http://en.wikipedia.org/wiki/DBm)
+    # The following is a convenient way to indicate, for example, that -85 is weaker than -10
     return 100 + int(string)
     
 def getpath():
-    if sys.platform=='linux2' and os.path.exists('/usr/bin/olinmm.py') and os.path.exists('/usr/share/olinmm'):
+    # XXX: Only works on Windows and Linux. This should be made private unless it is used in another file - Julian
+    if sys.platform.startswith('linux') and os.path.exists('/usr/bin/olinmm.py') and os.path.exists('/usr/share/olinmm'):
         return '/usr/share/olinmm/binutils/'
-    elif sys.platform=='linux2':
+    elif sys.platform.startswith('linux'):
         return 'binutils/'
     else:
         return '.\\binutils\\'
@@ -107,6 +139,6 @@ def getpath():
 
 if __name__ == '__main__':
     # test code
-    print getavgcoords(10, 0.15)
+    print getavgcoords(3, 0.15)
 
 
